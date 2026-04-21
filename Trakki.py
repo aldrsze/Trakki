159e9370c757ee79295b54cdd327c22b2bf0efdc
Aldrsze <Aldrinjariel0@gmail.com>
2026-04-21 12:14:47 +0800

    Refactor login and signup panels for improved user experience and input handling

--- Trakki.py
+++ Trakki.py
@@ -33,6 +33,10 @@ class Trakki:
 
     def clear(self):
         print(self.CLS, end="")
+    
+    def clear_buffer(self):
+        while msvcrt.kbhit():
+            msvcrt.getch()
 
     def __init__(self):
         self.expenses = []
@@ -63,31 +67,36 @@ class Trakki:
             print("\033[7;1H\033[K", end="")
             print("[ENTER] TO SUBMIT [R] TO RESET".center(w))
 
-            choice1 = msvcrt.getch().decode('utf-8').lower()
-            if choice1 == '\r':
-                for user in self.users:
-                    if username == user.username and password == user.password:
+            while True:
+                choice1 = msvcrt.getch().decode('utf-8').lower()
+                if choice1 == '\r':
+                    current_user = None
+                    for user in self.users:
+                        if username == user.username and password == user.password:
+                            current_user = username
+                            break
+
+                    if current_user:
                         print("\033[7;1H\033[K", end="")
                         print("LOGIN SUCCESSFUL!".center(w))
-                        time.sleep(0.5)
+                        self.dashboard_panel()
+                        return
                     else:
                         print("\033[7;1H\033[K", end="")
                         print("INVALID USERNAME OR PASSWORD.".center(w))
-                        msvcrt.getch()
-                        self.login_panel()
-            elif choice1 == 'r':
-                print("\033[7;1H\033[K", end="")
-                print("INPUT RESET SUCCESSFULLY! ".center(w))
-                msvcrt.getch()
-                self.signup_panel()
+                        self.clear_buffer()
+                        break
+                        
+                elif choice1 == 'r':
+                    break
+                else:
+                    pass
+            self.login_panel()
         elif choice == '2':
             return
         else:
-            print("\033[7;1H\033[K", end="")
-            print("INVALID CHOICE".center(w))
-            msvcrt.getch()
+            self.clear_buffer()
             self.login_panel()
-            
 
     def signup_panel(self):
         self.clear()
@@ -114,50 +123,47 @@ class Trakki:
             print("\033[7;1H\033[K", end="")
             print("[ENTER] TO SAVE [R] TO RESET".center(w))
 
-            choice1 = msvcrt.getch().decode('utf-8').lower()
+            while True:
+                choice1 = msvcrt.getch().decode('utf-8').lower()
+                
+                if choice1 == '\r': # \r is enter key
+                    user_exist = False
+                    for user in self.users:
+                        if username == user.username:
+                            user_exist = True
+                            break
+                    if user_exist:
+                        print("\033[7;1H\033[K", end="")
+                        print("USERNAME ALREADY EXIST".center(w))
+                        msvcrt.getch()
+                        time.sleep(0.1)
+                        self.signup_panel()
+                        return
+                    else:
+                        new_user = User(username, password)
+                        self.users.append(new_user)
+                        print("\033[7;1H\033[K", end="")
+                        print("ACCOUNT CREATED SUCCESSFULLY! ".center(w))
+                        msvcrt.getch()
+                        return
 
-            if choice1 == '\r': # \r is enter key
-                user_exist = False
-                for user in self.users:
-                    if username == user.username:
-                        user_exist = True
-                        break
-                if user_exist:
-                    print("\033[7;1H\033[K", end="")
-                    print("USERNAME ALREADY EXIST".center(w))
-                    msvcrt.getch()
+                elif choice1 == 'r':
                     self.signup_panel()
+                    return
                 else:
-                    new_user = User(username, password)
-                    self.users.append(new_user)
-                    print("\033[7;1H\033[K", end="")
-                    print("ACCOUNT CREATED SUCCESSFULLY! ".center(w))
-                    time.sleep(0.5)
-
-            elif choice1 == 'r':
-                print("\033[7;1H\033[K", end="")
-                print("INPUT RESET SUCCESSFULLY! ".center(w))
-                msvcrt.getch()
-                self.signup_panel()
-            else:
-                print("\033[7;1H\033[K", end="")
-                print(" INVALID CHOICE. ".center(w))
-                msvcrt.getch()
-                self.signup_panel()
+                    pass
         elif choice == '2':
             return
         else:
-            print("\033[7;1H\033[K", end="")
-            print(" INVALID CHOICE. ".center(w))
-            msvcrt.getch()
+            self.clear_buffer()
             self.signup_panel()
         
-        time.sleep(1)
     def about_panel(self):
         self.clear()
         input()
     def dashboard_panel(self):
         self.clear()
+        print("dashboard")
         input()
     def income_panel(self):
         self.clear()
@@ -185,9 +191,8 @@ class Trakki:
     def home_panel(self):
 
         w = 62
-        running = True
         
-        while running:
+        while True:
             self.clear()
             print("═"*w)
             text = """
@@ -214,16 +219,18 @@ class Trakki:
             elif choice == '3':
                 self.about_panel()
             elif choice == '0':
-                running = False
+                frames = [".  ", ".. ", "...", "   "]
+
+                for i in range(15):
+                    msg = f" Exiting {frames[i % 4]}"
+                    print(msg.center(w), end="\r")
+                    time.sleep(0.1)
+                print("\033[K", end="") # Clear the line
+                print("- Thank you for using the system -".center(w))
+                time.sleep(0.5)
+                return False
             else:
-                print("\033[9;1H\033[K", end="")
-                print("- INVALID CHOICE - ".center(w))
-                time.sleep(1)
-
-
-
-
-
+                self.clear_buffer()
 
 if __name__ == "__main__":
     Trakki = Trakki()

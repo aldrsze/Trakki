cd1b8204b42c64a01344e47793417ed4aa1f9f29
aldrsze <aldrinjariel0@gmail.com>
2026-04-30 15:02:29 +0800

    New model

--- logic/models.py
+++ logic/models.py
@@ -1,13 +1,18 @@
 from datetime import datetime
 
 class Expense:
-    def __init__(self, expense_id, amount, category, desc):
+    def __init__(self, expense_id, amount, category, desc, date_string=None):
         # Encapsulation
         self.__expense_id = expense_id
-        self.__amount = float(amount)
+        self.__amount = amount
         self.__category = category
         self.__desc = desc
-        self.__date = datetime.now().strftime("%B %d, %Y")
+
+        # If a custom date is provided, use it. Otherwise, use right now.
+        if date_string:
+            self.__date = date_string
+        else:
+            self.__date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     # Instance Methods (Getters) to safely read private data
     def get_expense_id(self): return self.__expense_id
     def get_amount(self): return self.__amount
@@ -16,7 +21,7 @@ class Expense:
     def get_date(self): return self.__date
 
     def set_amount(self, amount):
-        self.__amount = float(amount)
+        self.__amount = amount
 
     def set_category(self, category):
         self.__category = category
@@ -25,12 +30,17 @@ class Expense:
         self.__desc = desc
 
 class Income:
-    def __init__(self, income_id, amount, category, desc):
+    def __init__(self, income_id, amount, category, desc, date_string=None):
         self.__income_id = income_id
-        self.__amount = float(amount)
+        self.__amount = amount
         self.__category = category
         self.__desc = desc
-        self.__date = datetime.now().strftime("%B %d, %Y")
+        # If a custom date is provided, use it. Otherwise, use right now.
+        if date_string:
+            self.__date = date_string
+        else:
+            self.__date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     def get_income_id(self): return self.__income_id
     def get_amount(self): return self.__amount
     def get_category(self): return self.__category
@@ -38,7 +48,7 @@ class Income:
     def get_date(self): return self.__date
 
     def set_amount(self, amount):
-        self.__amount = float(amount)
+        self.__amount = amount
 
     def set_category(self, category):
         self.__category = category
@@ -47,12 +57,19 @@ class Income:
         self.__desc = desc
 
 class Target:
-    def __init__(self, target_id, name, cost, date, saved=0.00):
+    def __init__(self, target_id, name, cost, date, saved=0.00, created_at=None):
         self.__target_id = target_id
         self.__name = name
         self.__cost = float(cost)
         self.__saved = float(saved)
-        self.__date = date
+        self.__date = date # goal date
+
+        # creation date
+        if created_at:
+            self.__created_at = created_at
+        else:
+            self.__created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     def get_target_id(self): return self.__target_id
     def get_name(self): return self.__name
     def get_cost(self): return self.__cost
@@ -65,6 +82,7 @@ class Target:
         progress = (self.__saved / float(self.__cost)) * 100
         return max(0 ,min(100, progress)) # Caps at 100% and bottoms at 0%
     def get_date(self): return self.__date
+    def get_created_at(self): return self.__created_at
 
     def set_name(self, name):
         self.__name = name
